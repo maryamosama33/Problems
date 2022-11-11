97acdc4de1cd0dc38698a13145e1bf3bf2da904f
shahd osama hamed <102310738+shahdosama10@users.noreply.github.com>
2022-11-11 13:38:37 +0200

    problem 2 shahd

---
+++ problem2.cpp
@@ -0,0 +1,35 @@
+#include<bits\stdc++.h>
+#include "LabelGenerator.h"
+#include "FileLabelGenerator.h"
+using namespace std;
+int main(){
+    LabelGenerator figureNumbers("Figure ", 1);
+    LabelGenerator pointNumbers("P", 0);
+
+    cout << endl;
+    cout << "Figure numbers: ";
+    for (int i = 0; i < 3; i++) {
+        cout << figureNumbers.nextLabel() << ", ";
+    }
+    cout << endl;
+    cout << endl << "Point numbers: ";
+    for (int i = 0; i < 5; i++) {
+        cout << pointNumbers.nextLabel() << ", ";
+    }
+    cout << endl;
+    cout << endl << "More figures: ";
+    for (int i = 0; i < 3; i++) {
+        cout << figureNumbers.nextLabel() << ", ";
+    }
+    cout << endl;
+    cout<<"-----------------------------------------------------------------";
+    cout<<endl;
+    FileLabelGenerator figureLabels("Figure ", 1, "problem2.txt");
+    cout << "Figure labels of file: \n";
+    cout<<endl;
+    for (int i = 0; i < 3; i++) {
+        cout << figureLabels.nextLabel() << endl;
+        cout<<endl;
+
+    }
+}
